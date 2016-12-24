c38c767e27305196d8fcea8a06ef747f60731b62
SIMPLYBOYS <ferrari828@gmail.com>
2016-12-25 01:11:38 +0800

    implement search indexing function

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import pprint
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
 client = MongoClient('localhost', 27017)
 db = client['test']
 taiwan = db['taiwan']
 ptt = db['ptt']
-
 # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36'}
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
 headers = {'User-Agent': user_agent}
@@ -44,7 +45,7 @@ def parseTopic(url):
          post['link'] = "https://www.ptt.cc"+each_item.find('a', href=True)['href']
          post['title'] = each_item.select('div.title')[0].text.splitlines()[1]
          arrayDB.append(post)
-         inner.append({'title': post['title'], 'link': post['link']})
+         inner.append({'title': post['title'], 'link': post['link'], 'author': post['autor'], 'date': post['date']})
 
     result = ptt.insert_many(arrayDB)
     print('insert result: ' + str(result))
@@ -59,59 +60,89 @@ def getPost(title):
     document = ptt.find_one({'title': title})
     print(document)
     
-def updateDate_in_DB(title, year):
-      print(title+" "+year)
-      document = ptt.find_one({'title': title})
+def updateDate_in_DB(obj, year):
+      document = ptt.find_one({'title': obj['title']})
       print(document['date'])
-      ptt.update_one({
-          'title': title
+      size = len(document['date'].strip('"').split('/'))
+      date = document['date'].strip('"').split('/')[size-2:]
+      date = year+"/"+'/'.join(date)
+      ptt.update_many({
+          'title': obj['title']
       },{ 
           '$set':{
-              'date': year+"/"+document['date']
+              'date': date
            } 
       }, upsert=False)
+      document = ptt.find_one({'title': obj['title']})
+      searchIndexing(obj['title'], obj['link'], date, obj['author'], document['_id'])
+
+def resetDate_in_DB(obj):
+    print(obj['title']+" "+obj['year'])
+    document = ptt.find_one({'title': obj['title']})
+    size = len(document['date'].strip('"').split('/'))
+    bar = document['date'].strip('"').split('/')[size-2:]
+    date = '/'.join(bar)
+    ptt.update_one({
+        'title': obj['title']
+    },{
+        '$set':{
+            'date': date
+        }
+    }, upsert=False)
     
 def scrapPtt():
     pageLink = {}
-    i = 4929
-    end = 4934
+    i = 4934
+    end = 4944
     while i < end: 
         url = "https://www.ptt.cc/bbs/movie/index"+str(i)+".html"
         pageLink = parseTopic(url)
-        for index, link in enumerate(pageLink):
-            updatePostDate(link['link'], link['title'])
+        for index, page in enumerate(pageLink):
+            updatePostDate(page)
         i = i+1
 
 def updateDB():
     cursor = ptt.find({})
     for document in cursor:
-        updatePostDate(document['link'], document['title'])
-    print('update DB finished!')
+        updatePostDate(document)
+        print('update '+document['title']+' finished!')
                         
-def updatePostDate(url, title):
+def updatePostDate(obj):
     values = {}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')  
-    req = urllib.request.Request(url, data, headers)   
-    with urlopen(req) as response:
-       the_page = response.read()
-       bsObj = BeautifulSoup(the_page,"html.parser")
-       container = bsObj.select('.article-meta-value')
-       for innerindex, item in enumerate(container):
-           if (innerindex == 3):
-               if (item.text.find('(') == -1 or item.text.find(')') == -1):
-                    print(item.text[len(item.text)-4:])
-                    updateDate_in_DB(title, item.text[len(item.text)-4:])
-               else:
-                    item['text'] = item.text.strip("()").split('(')[1]
-                    print(item['text'][len(item['text'])-4:])
-                    updateDate_in_DB(title, item['text'][len(item['text'])-4:])
-                    
+    try:
+        req = urllib.request.Request(obj['link'], data, headers) 
+        with urlopen(req) as response:
+            the_page = response.read()
+            bsObj = BeautifulSoup(the_page,"html.parser")
+            container = bsObj.select('.article-meta-value')
+            for innerindex, item in enumerate(container):
+                if (innerindex == 3):
+                    if (item.text.find('(') == -1 or item.text.find(')') == -1):
+                         # print(item.text[len(item.text)-4:])
+                         updateDate_in_DB(obj, item.text[len(item.text)-4:])
+                    else:
+                         item['text'] = item.text.strip("()").split('(')[1]
+                         # print(item['text'][len(item['text'])-4:])
+                         updateDate_in_DB(obj, item['text'][len(item['text'])-4:])  
+    except urllib.error.HTTPError as err:
+        print(err.code)
+        return        
+    
+def searchIndexing(title, link, date, author, id):
+    doc = {
+        'link': link,
+        'title': title,
+        'date': date,
+        'author': author
+    }
+    res = es.index(index="ptt-test", doc_type='ptt', id=id, body=doc)
+    print(title+' indexing !')
+    return
+         
 # showPtt()
-# scrapPtt()
-updateDB()
+scrapPtt()
+#updateDB()
 # getPost("Re:[心得] 美國殺人魔結局")
-
-    
-
-    
+# searchIndexing()
